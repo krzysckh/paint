4deea76cefcdc7c44e590985788139bd3ee5c350
krzysckh <krzysckh@wp.pl>
2023-07-09 00:54:49 +0200

    actually add source to executable lmao

--- make.scm
+++ make.scm
@@ -1,6 +1,11 @@
 (use "make")
 ; no resources
 
+(define-resource "scm/colorscheme.scm")
+(define-resource "scm/init.scm")
+(define-resource "scm/paint.scm")
+(define-resource "scm/save.scm")
+
 (set-executable-name "paint")
 
 (make)
